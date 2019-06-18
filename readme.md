bd5c467e73c260622d8421db286579bad62d2c6b
Alex Christian <aquispe.developer@gmail.com>
2019-06-18 16:05:41 -0500

    Agregado documentacion para la comunidad y orden del codigo

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Script C# para realizar una captura de pantalla en windows
+
+El proposito de este repositorio es realizar capturas de pantalla en windows >= 7.
+
+## Pasos
+``` bash
+# Clonar proyecto de github
+git clone https://github.com/acqrdeveloper/capture-screenshot-window.git screenshot-windows
+
+# Ubicarse en la carpeta temporal del sistema windows
+C:\Users\[my_user]\AppData\Local\Temp
+
+# Ejecutar screenshot-windows.exe ubicado en la carpeta bin\debug
+D:\screenshot-windows\screenshot-windows\bin\Debug
+``` 
---
+++ screenshot-windows/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screenshot_windows
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Disabled runner application window form
+            //Application.Run(new Form1());
+
+            //Start capture screenshot
+            String filename = "img_" + DateTime.Now.ToString("ddMMyyyy") + "_" + DateTime.Now.ToString("hhmmss");
+            ScreenCapturer.CaptureAndSave(filename);
+        }
+    }
+}
